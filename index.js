b9edfd461ea508a11f0b413efb04b0fa3990a4e6
SteadFast <sfast0258@gmail.com>
2024-08-15 15:40:53 +0100

    Refactor code to using express framework

--- index.js
+++ index.js
@@ -1,57 +1,114 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+// const http = require('http');
+// const url = require('url');
+// const fs = require('fs');
 
-const server = http.createServer();
+// const server = http.createServer();
 
-server.on('request', (req, res) => {
-  const urlPath = url.parse(req.url, true);
+// server.on('request', (req, res) => {
+//   const urlPath = url.parse(req.url, true);
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
 
-  if (urlPath.pathname === '/') {
-    fs.readFile('./index.html', 'utf-8', (err, data) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+//   if (urlPath.pathname === '/') {
+//     fs.readFile('./index.html', 'utf-8', (err, data) => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
 
-      return res.end(data);
-    });
-  }
+//       return res.end(data);
+//     });
+//   }
 
-  if (urlPath.pathname === '/about') {
-    fs.readFile('./about.html', 'utf-8', (err, data) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+//   if (urlPath.pathname === '/about') {
+//     fs.readFile('./about.html', 'utf-8', (err, data) => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
 
-      return res.end(data);
-    });
-  }
+//       return res.end(data);
+//     });
+//   }
 
-  if (urlPath.pathname === '/contact-me') {
-    fs.readFile('./contact-me.html', 'utf-8', (err, data) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+//   if (urlPath.pathname === '/contact-me') {
+//     fs.readFile('./contact-me.html', 'utf-8', (err, data) => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
 
-      return res.end(data);
-    });
-  }
+//       return res.end(data);
+//     });
+//   }
 
-  fs.readFile('./404.html', 'utf-8', (err, data) => {
+//   fs.readFile('./404.html', 'utf-8', (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+
+//     return res.end(data);
+//   });
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server running on port 3000');
+// });
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const options = {
+  root: path.join(__dirname, 'public'),
+  headers: {
+    'Content-Type': 'text/html',
+  },
+};
+
+app.get('/', (req, res) =>
+  res.sendFile('index.html', options, (err) => {
     if (err) {
-      console.log(err);
-      return;
+      next(err);
+    } else {
+      console.log('Sent: index.html');
     }
+  })
+);
 
-    return res.end(data);
-  });
-});
+app.get('/about', (req, res) =>
+  res.sendFile('about.html', options, (err) => {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent: about.html');
+    }
+  })
+);
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+app.get('/contact-me', (req, res) =>
+  res.sendFile('contact-me.html', options, (err) => {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent: contact.html');
+    }
+  })
+);
+
+app.get('*', (req, res) =>
+  res.sendFile('404.html', options, (err) => {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent: 404.html');
+    }
+  })
+);
+
+const PORT = 3000;
+app.listen(PORT, 'localhost', () =>
+  console.log(`Server runing on PORT: ${PORT}`)
+);
